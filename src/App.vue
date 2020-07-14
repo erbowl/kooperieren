ea0eb0c42137de917d19db9baf09d16ab2d1531a
Keiichiro Seto <shostakovich24@gmail.com>
2020-07-14 12:04:25 +0900

    いらんもんがありそう

--- src/App.vue
+++ src/App.vue
@@ -33,21 +33,17 @@
       </v-btn>
     </v-app-bar>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+    <v-content> </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+// import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
 
-  components: {
-    HelloWorld
-  },
+  components: {},
 
   data: () => ({
     //
