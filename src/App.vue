b73f298e6ef44040ebb81990b881db0c81ea0f68
Keiichiro Seto <shostakovich24@gmail.com>
2020-07-14 12:11:03 +0900

    コンポーネント化すると壊れる

--- src/App.vue
+++ src/App.vue
@@ -34,12 +34,26 @@
     </v-app-bar>
 
     <v-content> </v-content>
+    <v-bottom-navigation>
+      <v-btn value="recent">
+        <span>Recent</span>
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn value="favorites">
+        <span>Favorites</span>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn value="nearby">
+        <span>Nearby</span>
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
 
